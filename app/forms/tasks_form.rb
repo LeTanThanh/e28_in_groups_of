2ee65c91a9496ba0c5d66d7a201cc59436173f22
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2019-08-25 20:06:30 +0700

    fix_errors

--- app/forms/tasks_form.rb
+++ app/forms/tasks_form.rb
@@ -4,10 +4,14 @@ class TasksForm
   end
 
   def tasks_number
-    @tasks_number ||= params[:tasks][:tasks_number].to_i || Settings.task.default_tasks_number
+    @tasks_number ||= params_tasks_number || Settings.task.default_tasks_number
   end
 
   private
-
   attr_reader :params
+
+  def params_tasks_number
+    tasks_number = params.dig(:tasks, :tasks_number).to_i
+    tasks_number.positive? ? tasks_number : nil
+  end
 end
